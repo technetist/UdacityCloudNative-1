2c4b8a2f404d5e2d0865afb4d08364ee779e0bff
Adrien <adrien@maranville.io>
2021-06-10 17:13:33 -0600

    adds metric and status routes

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,9 +1,18 @@
-from flask import Flask
+from flask import Flask, Response
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello developer!"
+
+@app.route("/status")
+def status():
+    return Response('{"result": "OK - healthy"}', status=200, mimetype='application/json')
+
+@app.route("/metrics")
+def metrics():
+    return Response('{"data": {"UserCount": 140, "UserCountActive": 23}}', status=200, mimetype='application/json')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
