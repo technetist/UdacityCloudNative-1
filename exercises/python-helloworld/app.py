9457ce22840c3d343ec035b116f325d998b5ca7a
Adrien <adrien@maranville.io>
2021-06-10 20:04:00 -0600

    adds logs for endpoints

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,6 +1,13 @@
-from flask import Flask, Response
+from flask import Flask, Response, request
+import logging
 
 app = Flask(__name__)
+logging.basicConfig(
+    filename='app.log',
+    level=logging.INFO,
+    format= '%(asctime)s, %(message)s',
+    datefmt='%m/%d/%Y %H:%M:%S'
+)
 
 @app.route("/")
 def hello():
@@ -8,10 +15,12 @@ def hello():
 
 @app.route("/status")
 def status():
+    app.logger.info(f'{request.path} endpoint was reached')
     return Response('{"result": "OK - healthy"}', status=200, mimetype='application/json')
 
 @app.route("/metrics")
 def metrics():
+    app.logger.info(f'{request.path} endpoint was reached')
     return Response('{"data": {"UserCount": 140, "UserCountActive": 23}}', status=200, mimetype='application/json')
 
 if __name__ == "__main__":
